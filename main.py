16d65e0d6d5a92a8146e18224b4ab4a476a31d03
nvhgamer <nielsvanhecke@hotmail.nl>
2025-11-12 17:36:29 +0100

    Chapter 1 finished

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    verbose = False
+
+    non_flag_tokens = []
+
+    # Parse command-line arguments by looping through sys.argv
+    for a in args:
+        # Check if there is a "--verbose" flag
+        if a == "--verbose":
+            verbose = True
+        # Check for other flags starting with "--"
+        elif a.startswith("--"):
+            # ignore unknown flags for now
+            continue
+        # Otherwise, treat it as part of the prompt
+        else:
+            non_flag_tokens.append(a)
+
+    # Ensure that there is at least one non-flag argument (the prompt)
+    if not non_flag_tokens:
+        print("Error: No prompt provided. Usage: uv run main.py \"<prompt>\" [--verbose]")
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(non_flag_tokens)
+
+    if verbose:
+        print(f'User prompt: {user_prompt}')
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose=False):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
